623cdcd66c8e108e5befc38512d3b23090e1f090
Catalin B <catalin.bac@gmail.com>
2022-10-27 15:04:21 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ getHamburger.addEventListener('click', () => {
   getNav.classList.toggle('show');
 });
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('#main', {
   // cssMode: true,
   loop: true,
   // centeredSlides: true,
@@ -46,4 +46,53 @@ const swiper = new Swiper('.swiper', {
   clickable: true,
   // mousewheel: true,
   // keyboard: true,
+});
+
+const swiperTestimonial = new Swiper('#second', {
+  // cssMode: true,
+  loop: true,
+  // centeredSlides: true,
+  allowTouchMove: true,
+  slidesPerView: 1,
+  // spaceBetween: 10,
+  // autoHeight: true,
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+  // speed: 2000,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  //   pauseOnMouseEnter: true,
+  // },
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   dynamicBullets: true,
+
+  // },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 640px
+    1280: {
+      slidesPerView: 1,
+    },
+  },
+  clickable: true,
+  // mousewheel: true,
+  // keyboard: true,
+
+});
+
+AOS.init({
+  // duration: 400,
+  delay: 100,
+  easing: 'ease-in-sine',
 });
