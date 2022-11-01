2c731b4b2d303edf57191528e829f531bacf6e63
Catalin B <catalin.bac@gmail.com>
2022-11-01 10:24:54 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -20,7 +20,7 @@ const swiper = new Swiper('#main', {
   // },
   speed: 2000,
   autoplay: {
-    delay: 2500,
+    delay: 2000,
     disableOnInteraction: false,
     pauseOnMouseEnter: true,
   },
@@ -74,15 +74,15 @@ const swiperTestimonial = new Swiper('#second', {
   breakpoints: {
     // when window width is >= 320px
     320: {
-      slidesPerView: 1,
+      slidesPerView: 2,
     },
     // when window width is >= 480px
     768: {
-      slidesPerView: 1,
+      slidesPerView: 3,
     },
     // when window width is >= 640px
     1280: {
-      slidesPerView: 1,
+      slidesPerView: 4,
     },
   },
   clickable: true,
